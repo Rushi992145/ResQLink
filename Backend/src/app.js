cd375e6a0169073b4cca6c7d3cac06929c5aee2a
rushi992145 <rushikeshiray99@gmail.com>
2025-03-22 16:44:07 +0530

    temp

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,12 +17,12 @@ app.use(cookieParser())
 
 //routes import
 
-import userRouter from "./routes/user.routes.js"
-import postRouter from "./routes/post.routes.js"
+// import userRouter from "./routes/user.routes.js"
+// import postRouter from "./routes/post.routes.js"
 
 //routes declaration
-app.use("/api/v2/users",userRouter)
-app.use("/api/v2/post",postRouter)
+// app.use("/api/v2/users",userRouter)
+// app.use("/api/v2/post",postRouter)
 app.get('/api/health', (req, res) => {
     res.status(200).json({ message: "Service is healthy!" });
 });
