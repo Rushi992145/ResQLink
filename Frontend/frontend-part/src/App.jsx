359b47932825b029e3a89be98cbc71508951ac3c
brijmohan17 <brijmohangour895@gmail.com>
2025-03-22 14:43:06 +0530

    setup of frontend

---
+++ Frontend/frontend-part/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='font-extrabold flex justify-center '>
+
+      Hello World
+    </div>
+    </>
+  )
+}
+
+export default App
