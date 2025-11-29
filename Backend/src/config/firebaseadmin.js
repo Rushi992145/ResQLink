949a938afa9cc7308c07acd16584739801d4b221
OnkarJondhale <onkar.jondhale@gmail.com>
2025-11-30 00:16:36 +0530

    Notification feature added

--- Backend/src/config/firebaseadmin.js
+++ Backend/src/config/firebaseadmin.js
@@ -8,22 +8,21 @@ dotenv.config();
 // import serviceAccount from '../reqlink123-792ea-firebase-adminsdk-fbsvc-57f7170f2f.json' assert { type: 'json' };
 
 // Use this for developement environment
-// const require = createRequire(import.meta.url);
-// const serviceAccount = require("../reqlink123-792ea-firebase-adminsdk-fbsvc-57f7170f2f.json");
-
+const require = createRequire(import.meta.url);
+const serviceAccount = require("../reqlink123-792ea-firebase-adminsdk-fbsvc-903702d8d8.json");
 
 // Below thing is under testing purpose 
-let serviceAccount;
+// let serviceAccount;
 
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  // Parse JSON string from environment variable
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-} else {
-  // Fallback for local dev: load from file (ignored in .gitignore)
-  const { createRequire } = await import("module");
-  const require = createRequire(import.meta.url);
-  serviceAccount = require("../reqlink123-792ea-firebase-adminsdk-fbsvc-57f7170f2f.json");
-}
+// if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+//   // Parse JSON string from environment variable
+//   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// } else {
+//   // Fallback for local dev: load from file (ignored in .gitignore)
+//   const { createRequire } = await import("module");
+//   const require = createRequire(import.meta.url);
+//   serviceAccount = require("../reqlink123-792ea-firebase-adminsdk-fbsvc-57f7170f2f.json");
+// }
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
